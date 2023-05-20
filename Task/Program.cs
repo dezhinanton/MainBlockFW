f5be5824ed193dd88a13ded40bfae98b7c62188f
Dezhin Anton <dezhin@email.com>
2023-05-20 16:38:32 +0300

    Task Done

---
+++ Task/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+string[]array1 = new string[7] {"final", "5", "-5", "98432", "Kazan", ":-)", "smile"};
+string[]array2 = new string[array1.Length];
+void FindSecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Результат: ");
+FindSecondArray(array1, array2);
+PrintArray(array2);
